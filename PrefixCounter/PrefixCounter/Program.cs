2b5100d972cdfd68af7222b811adef1dbf377786
Joel Makara <makarajm@gmail.com>
2019-05-11 10:53:00 +0200

    Check in of the PrefixCounter and SortedListMerger programming challenge solutions

---
+++ PrefixCounter/PrefixCounter/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrefixCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Steven", "Stevens", "Hulk", "Hulky" };
+            var query = new List<string> { "Steve", "Hulk", "Tom" };
+            var result = FindPrefix(names, query);
+            PrintList<int>(result);
+            Console.ReadLine();
+        }
+                
+
+        private static List<int> FindPrefix(List<string> names, List<string> query)
+        {
+            var output = new List<int>();
+
+            for(int j = 0; j < query.Count; j++)
+            {
+                var count = 0;
+
+                for(int k=0; k < names.Count; k++)
+                {
+                    if (names[k].Length < query[j].Length || names[k] == query[j])
+                    {
+                        continue;
+                    }
+                    
+                    if( names[k].Substring(0,query[j].Length) == query[j])
+                    {
+                        count++;
+                    }
+                }
+                output.Add(count);
+            }
+
+            return output;
+        }
+
+        private static void PrintList<T>(List<T> inputList)
+        {
+            Console.Write("{");
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if( i == inputList.Count -1)
+                {
+                    Console.Write($" {inputList[i]}");
+                }
+                else
+                {
+                    Console.Write($" {inputList[i]},");
+                }
+            }
+            Console.Write("} \n");
+        }
+    }
+}
---
+++ SortedListMerger/SortedListMerger/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedListMerger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstSortedList = new List<int> { 1, 2,3,4 };
+            var secondSortedList = new List<int> { 0, 1, 5, 7, 7 };
+            var result = MergeLists(firstSortedList, secondSortedList);
+            PrintList<int>(result);
+            Console.ReadLine();
+        }
+
+        private static List<int> MergeLists(List<int> firstSortedList, List<int> secondSortedList)
+        {
+            var outputList = new List<int>();
+            var firstListIterator = 0;
+
+            while (firstListIterator < firstSortedList.Count)
+            {
+                var listElement = firstSortedList[firstListIterator];
+                if (secondSortedList.Count == 0)
+                {
+                    outputList.Add(listElement);
+                    firstListIterator++;
+                    continue;
+                }
+
+                var comparator = secondSortedList[0];
+
+                if (comparator <= listElement)
+                {
+                    outputList.Add(comparator);
+                    secondSortedList.Remove(comparator);
+                    continue;
+                }
+
+                outputList.Add(listElement);
+                firstListIterator++;
+            }
+            outputList.AddRange(secondSortedList);
+            return outputList;
+        }
+
+        private static void PrintList<T>(List<T> inputList)
+        {
+            Console.Write("{");
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (i == inputList.Count - 1)
+                {
+                    Console.Write($" {inputList[i]}");
+                }
+                else
+                {
+                    Console.Write($" {inputList[i]},");
+                }
+            }
+            Console.Write("} \n");
+        }
+    }
+}
